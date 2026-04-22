55f9396f6b8b918640de1ffd4abbdba5f4adabfa
parthkavathiya26 <pckavathiya1@gmail.com>
2026-04-22 22:36:42 +0530

    deployment setup

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from backend.cart.models import CartItem 
+from django.http import HttpResponse
 
 import random
 
